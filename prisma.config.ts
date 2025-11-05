154a190a2ceed6019a2cf668d8e9eb3876833901
dipSyxx <linetsky.yura@gmail.com>
2025-11-05 13:05:13 +0100

    Refactor Prisma configuration to utilize dotenv for environment variables and define schema and migrations paths.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,1 +1,13 @@
-export default {};
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
