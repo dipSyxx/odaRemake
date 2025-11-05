817f3ecf58ef476df73758edddf39d6f2b57c10a
dipSyxx <linetsky.yura@gmail.com>
2025-11-05 13:01:25 +0100

    Update Next.js configuration to ignore ESLint during builds and set outputFileTracingRoot. Simplify Prisma configuration to an empty export.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+  // Fix for multiple lockfiles warning
+  outputFileTracingRoot: process.cwd(),
+};
 
-export default nextConfig
+export default nextConfig;
